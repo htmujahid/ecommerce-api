e7f237928a41fbe31295b9ee58ff7e384779c2d0
htmujahid <htmujahid@gmail.com>
2023-08-19 09:25:13 +0500

    refact: exception handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,11 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +30,39 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     */
+    public function render($request, Throwable $e)
+    {
+        if ($request->expectsJson() && $e instanceof AuthorizationException) {
+            return response()->json([
+                'message' => 'You are not authorized to access this resource',
+            ], Response::HTTP_FORBIDDEN);
+        }
+
+        else if ($request->expectsJson() && $e instanceof ModelNotFoundException) {
+            return response()->json([
+                'message' => 'The resource was not found in the database',
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        return parent::render($request, $e);
+    }
+
+    /**
+     * Convert an authentication exception into an unauthenticated response.
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json([
+                'message' => 'You are not authenticated to access this resource',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return redirect()->guest(route('login'));
     }
 }
