94ba55b858aca078be5961bb4c0bf325d63a7262
htmujahid <htmujahid@gmail.com>
2023-09-01 08:12:08 +0500

    fix: category schema

--- database/migrations/2023_08_18_164835_create_categories_table.php
+++ database/migrations/2023_08_18_164835_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
-            $table->string('description', 255);
+            $table->string('description', 255)->nullable();
             $table->string('slug', 100)->unique();
             $table->foreignId('parent_id')->nullable()->constrained('categories');
             $table->boolean('status')->default(true);
